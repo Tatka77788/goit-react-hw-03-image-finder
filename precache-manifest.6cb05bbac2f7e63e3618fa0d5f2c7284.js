6448a60ffe7ab74d0c6cd6f3c38166a67de263d5
Tetiana <tatka777t@gmail.com>
2020-05-08 22:06:10 +0300

    Updates

--- precache-manifest.6cb05bbac2f7e63e3618fa0d5f2c7284.js
+++ precache-manifest.6cb05bbac2f7e63e3618fa0d5f2c7284.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1bcd5a3950f24cfe6b533a6355fc7f73",
+    "revision": "34512acf4917f7432e17e574a609c1c2",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/css/2.e50de653.chunk.css"
   },
   {
-    "revision": "9fef1e30eefff39d9c3c",
-    "url": "/goit-react-hw-03-image-finder/static/css/main.47f67dd1.chunk.css"
+    "revision": "6531bd066f331be77eab",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.e540c6e2.chunk.css"
   },
   {
     "revision": "85f723c9157f62e8e272",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/js/2.4696afb7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9fef1e30eefff39d9c3c",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.55f08365.chunk.js"
+    "revision": "6531bd066f331be77eab",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.dc76c58d.chunk.js"
   },
   {
     "revision": "ef7f04d3f12739069562",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.d8e3ec782b5522fe496d7be21f815589.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.6cb05bbac2f7e63e3618fa0d5f2c7284.js"
 );
 
 self.addEventListener('message', (event) => {
