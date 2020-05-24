0817b6bb8aed2a16245a4dcae6d51f39a76c1aeb
Tetiana <tatka777t@gmail.com>
2020-05-24 15:19:52 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.ee51c81cf4bca02f88cc24bcbd98eac2.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.415a66bc562254cbce11ea10d99bb40d.js"
 );
 
 self.addEventListener('message', (event) => {
